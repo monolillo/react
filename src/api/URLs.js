0b1b3f4b39a16ba3eb9ae0a77f9b9d1bc00f3222
Rodrigo Masanes <rodrigo.masanes@neoris.com>
2018-07-13 10:02:57 -0300

    Working groups modules

--- src/api/URLs.js
+++ src/api/URLs.js
@@ -7,6 +7,9 @@ export const API_URL_PEOPLE = 'rest/people';
 export const API_URL_SKILLS = 'rest/skills';
 export const API_URL_STATION = 'rest/station';
 export const API_URL_GROUPS = 'rest/groups';
+export const API_URL_NEW = '/new';
+export const API_URL_UPDATE = '/update';
+export const API_URL_DELETE = '/delete';
 
 // Internal routes
 export const URL_LOGIN = 'login';
--- src/components/groups/index.js
+++ src/components/groups/index.js
@@ -1,7 +1,10 @@
 import React, { Component, Fragment } from 'react';
-import { Input, Button, Form, Grid, Segment, Header, Confirm } from 'semantic-ui-react';
+import { Input, Button, Form, Grid, Segment, Header, Confirm, Dropdown } from 'semantic-ui-react';
 import SearchComponent from '../search';
 import { connect } from 'react-redux'
+import Request from '../../api/request';
+import { API_URL_GROUPS, API_URL_NEW, API_URL_UPDATE, API_URL_DELETE } from '../../api/URLs';
+import NotificationSystem from 'react-notification-system';
 
 class Groups extends Component {
 
@@ -11,7 +14,9 @@ class Groups extends Component {
         this.state = {
             id: 0,
             name: '',
-            nextProps: []
+            nextProps: [],
+            loader: true,
+            users: []
         };
     }
 
@@ -42,21 +47,109 @@ class Groups extends Component {
         this.clearState();
     }
 
+    loadUsers() {
+
+    }
+
+    handleCreate = async () => {
+
+        const { name } = this.state;
+
+        const details = {
+            name: name
+        }
+
+        Request.post({
+            url: API_URL_GROUPS + API_URL_NEW,
+            data: details
+        }).then(response => {
+            if(response.status === 201){
+                this.refs.notificationSystem.addNotification({
+                    message: 'Success',
+                    level: 'success',
+                    position: 'br'
+                });
+                this.clearState();
+            }
+        }).catch(error => {
+            this.refs.notificationSystem.addNotification({
+                message: 'There was an unexpected situation, try again later',
+                level: 'warning',
+                position: 'br'
+            });
+        })
+    }
+
+    handleUpdate = async () => {
+
+        const { id, name } = this.state;
+
+        const details = {
+            name: name
+        }
+
+        Request.put({
+            url: API_URL_GROUPS + '/' + id + API_URL_UPDATE,
+            data: details
+        }).then(response => {
+            if(response.status === 204){
+                this.refs.notificationSystem.addNotification({
+                    message: 'Success',
+                    level: 'success',
+                    position: 'br'
+                });
+                this.clearState();
+            }
+        }).catch(error => {
+            this.refs.notificationSystem.addNotification({
+                message: 'There was an unexpected situation, try again later',
+                level: 'warning',
+                position: 'br'
+            })
+        })
+    }
+
+    handleDelete = async () => {
+
+        const { id } = this.state;
+
+        Request.delete({
+            url: API_URL_GROUPS + '/' + id + API_URL_DELETE
+        }).then(response => {
+            if(response.status === 200){
+                this.refs.notificationSystem.addNotification({
+                    message: 'Success',
+                    level: 'success',
+                    position: 'br'
+                });
+                this.clearState();
+            }
+        }).catch(error => {
+            this.refs.notificationSystem.addNotification({
+                message: 'There was an unexpected situation, try again later',
+                level: 'warning',
+                position: 'br'
+            });
+        })
+    }
+
     render() {
         return (
             <div>
                 <Fragment>
-                    <Grid>
-                        <Grid.Row>
-                            <Grid.Column textAlign='right'>
-                                <SearchComponent />
-                            </Grid.Column>
-                        </Grid.Row>
-                    </Grid>
                     <Segment raised>
-                        <Header as='h2'>
-                            <Header.Content> Groups </Header.Content>
-                        </Header>
+                        <Grid>
+                            <Grid.Row columns={2}>
+                                <Grid.Column textAlign='left'>
+                                    <Header as='h2'>
+                                        <Header.Content> Groups </Header.Content>
+                                    </Header>
+                                </Grid.Column>
+                                <Grid.Column textAlign='right'>
+                                    <SearchComponent />
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
                         <Form>
                             <Form.Group>
                                 <Form.Field width={3}>
@@ -81,9 +174,62 @@ class Groups extends Component {
                                 />
                                 </Form.Field>
                             </Form.Group>
+                            <Form.Field>
+                                <label>User Selection</label>
+                                <Dropdown
+                                disabled={this.state.id === 0 ? true : false}
+                                multiple
+                                selection
+                                fluid
+                                options={skills}
+                                placeholder='Choose one or multiple skills'
+                                renderLabel={renderLabel}
+                                search
+                                loading={loadingSkills}
+                                onChange={(e, { value }) => this.changeValue('selectedSkills', value)}
+                                value={selectedSkills}
+                                />
+                            </Form.Field>
+                            {this.state.id === 0 || this.state.id === undefined ? //New People
+                                <Grid>
+                                    <Grid.Row columns={2}>
+                                        <Grid.Column></Grid.Column>
+                                        <Grid.Column textAlign='right'>
+                                        <Form.Group>
+                                            <Form.Field width={8}>
+                                            <Button fluid primary onClick={this.handleCreate}>Create</Button>
+                                            </Form.Field>
+                                            <Form.Field width={8}>
+                                            <Button fluid color='grey' onClick={this.clearState}>Clear</Button>
+                                            </Form.Field>
+                                        </Form.Group>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
+                                : //Existent people
+                                <Grid>
+                                    <Grid.Row columns={2}>
+                                        <Grid.Column></Grid.Column>
+                                        <Grid.Column textAlign='right'>
+                                        <Form.Group>
+                                            <Form.Field width={6}>
+                                            <Button fluid primary onClick={this.handleUpdate}>Update</Button>
+                                            </Form.Field>
+                                            <Form.Field width={6}>
+                                            <Button fluid color='red' onClick={this.handleDelete}>Delete</Button>
+                                            </Form.Field>
+                                            <Form.Field width={6}>
+                                            <Button fluid color='grey' onClick={this.clearState}>Clear</Button>
+                                            </Form.Field>
+                                        </Form.Group>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
+                            }
                         </Form>
                     </Segment>
                 </Fragment>
+                <NotificationSystem ref="notificationSystem" />
             </div>
         );
     }
